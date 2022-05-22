0bb15be212c64a92fb0227a724f4ac528f903e26
Fossu <a.brejzek@gmail.com>
2022-05-22 17:22:17 +0200

    Make js skeleton

--- rps.js
+++ rps.js
@@ -1,8 +1,4 @@
 // Open a prompt window and get a player selection
-function getSelection() {
-    const selection = window.prompt("What do you play?", "");
-    return selection;
-}
 
 function computerPlay() {
     const options = ['rock', 'paper', 'scissors'];
@@ -13,42 +9,69 @@ function computerPlay() {
 
 function playResults(playerSelection, computerSelection) {
     const options = ['rock', 'paper', 'scissors'];
-    // Standardize strings as we care only about the letters not capitalization
-    const computer_lower = computerSelection.toLowerCase();
-    const player_lower = playerSelection.toLowerCase();
-    const player_index = options.indexOf(player_lower);
-    const computer_index = options.indexOf(computer_lower);
-    if (player_index == -1) {
-        return "Error - check spelling of player selection.";
-    }
-    const player_choice_pretty = player_lower.charAt(0).toUpperCase() + player_lower.slice(1);
-    const computer_choice_pretty = computer_lower.charAt(0).toUpperCase() + computer_lower.slice(1);
+    const player_index = options.indexOf(playerSelection);
+    const computer_index = options.indexOf(computerSelection);
+
+    const player_choice_pretty = playerSelection.charAt(0).toUpperCase() +
+        playerSelection.slice(1);
+    const computer_choice_pretty = computerSelection.charAt(0).toUpperCase() +
+        computerSelection.slice(1);
+
     if (player_index === computer_index) {
         console.log("It's a Tie");
+        currentGame.textContent = "It's a tie";
         return 0;
         // Cool trick to not make 9 different conditions
     } else if (computer_index === (player_index + 1) % 3) {
-        console.log('You Lose! ' + computer_choice_pretty + ' beats ' + player_choice_pretty);
+        console.log('You Lose! ' + computer_choice_pretty + ' beats ' +
+            player_choice_pretty);
+        currentGame.textContent = 'You Lose! ' + computer_choice_pretty + ' beats ' +
+            player_choice_pretty;
         return -1;
     } else {
-        console.log('You win! ' + player_choice_pretty + ' beats ' + computer_choice_pretty);
+        console.log('You win! ' + player_choice_pretty + ' beats ' +
+            computer_choice_pretty);
+        currentGame.textContent = 'You win! ' + player_choice_pretty + ' beats ' +
+            computer_choice_pretty;
         return 1;
     }
 }
 
-function game() {
-    let player_score = 0;
-    for (let i = 0; i < 5; i++) {
-        playerSelection = getSelection();
-        computerSelection = computerPlay();
-        result = playResults(playerSelection, computerSelection);
-        player_score = player_score + result
-    }
-    if (player_score === 0) {
-        console.log('Game ended in a tie');
-    } else if (player_score > 0) {
-        console.log('Player won!');
-    } else {
-        console.log('Computer won!');
-    }
+function singleGame(e) {
+    const computerSelection = computerPlay();
+    console.log(computerSelection);
+    const playerSelection = this.getAttribute('id');
+    const roundResult = playResults(playerSelection, computerSelection);
+    updateResults(roundResult);
 }
+
+function updateResults(roundResult) {
+    playerScore = playerScore + roundResult;
+    gamesPlayed++;
+    if (gamesPlayed === 5) {
+        if (playerScore === 0) {
+            console.log('Game ended in a tie');
+            gameResults.textContent = 'Game ended in a tie';
+        } else if (playerScore > 0) {
+            console.log('Player won!');
+            gameResults.textContent = 'Player won!';
+        } else {
+            console.log('Computer won!');
+            gameResults.textContent = 'Comupter won!';
+        }
+        playerScore = 0;
+        gamesPlayed = 0;
+    }
+    return playerScore;
+}
+
+let playerScore = 0;
+let gamesPlayed = 0;
+
+const currentGame = document.querySelector('.current');
+const wins = document.querySelector('.wins');
+const loses = document.querySelector('.loses');
+const gameResults = document.querySelector('.gameResults');
+
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', singleGame));
